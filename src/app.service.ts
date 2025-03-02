7b2e1237f1f636de34ea171a0d097b4926ff9139
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-01 23:22:45 -0500

    update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common'
 export class AppService {
   getHello() {
     return {
-      message: 'ok',
+      message: 'Hello World!',
       status: 200,
       data: [],
     }
