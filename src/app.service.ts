aa17d943e257553b037d14200edf877817740823
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-03 23:45:51 -0500

    update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common'
+import { Task } from './entitie/example.dto'
 
 @Injectable()
 export class AppService {
   getHello() {
+    const example = new Task(1, 'title', 'description', true)
     return {
       message: 'Hello World!',
       status: 200,
-      data: [],
+      data: example,
     }
   }
 }
