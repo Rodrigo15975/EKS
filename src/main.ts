68550c443bb4b85cd0f92cbb14149e8bd490ea34
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-07 20:54:19 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const PORT: number = Number(process.env.PORT) || 3000
+  await app.listen(PORT)
 }
 bootstrap()
   .then(() => console.log('Server started', process.env.PORT))
