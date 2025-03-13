22a403cc642f7c32af91749e1b553f885cda5a65
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-12 23:50:50 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as fs from 'fs'
-import { join } from 'path'
+// import * as fs from 'fs'
+// import { join } from 'path'
 import { Logger } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = 3000
 
   // Leer secretos montados por el CSI Driver
-  const secretsPath = '/mnt/secrets-store'
-  const dbUser = fs.readFileSync(join(secretsPath, 'db-user'), 'utf8')
-  const dbPassword = fs.readFileSync(join(secretsPath, 'db-password'), 'utf8')
+  // const secretsPath = '/mnt/secrets-store'
+  // const dbUser = fs.readFileSync(join(secretsPath, 'db-user'), 'utf8')
+  // const dbPassword = fs.readFileSync(join(secretsPath, 'db-password'), 'utf8')
 
-  Logger.debug(`Database User: ${dbUser}`) // Imprime: admin
-  Logger.debug(`Database Password: ${dbPassword}`) // Imprime: securepassword123
+  // Logger.debug(`Database User: ${dbUser}`) // Imprime: admin
+  // Logger.debug(`Database Password: ${dbPassword}`) // Imprime: securepassword123
 
   await app.listen(PORT, () => {
     Logger.verbose(`Server running on port ${PORT}`)
